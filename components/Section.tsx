5a07322848c88bff549573e25247c4a1df142918
simdi jinkins <sim04ful@gmail.com>
2019-09-04 12:48:59 +0100

    working on layout

---
+++ components/Section.tsx
@@ -0,0 +1,5 @@
+import { Row } from 'react-bootstrap';
+
+export default ({ children }) => {
+    return <Row></Row>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+
+import Button from '../components/Button';
+import Section from '../components/Section';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default () => {
-    return (<div>
-        <p>Hello</p>
+    return (
+        <Container fluid>
+            <Row >
+                <Col style={{ backgroundColor: 'white', padding: 20 }} sm={12} md={4}>Hello</Col>
+                <Col sm={12} md={8}>
+                    <Row>
+                        <Col style={{ backgroundColor: 'blue', padding: 20 }} sm={12} >Hello</Col>
+                        <Col style={{ backgroundColor: 'orange', padding: 20 }} sm={12} >Hello</Col>
+                    </Row>
 
-    </div>)
+                </Col>
+
+
+            </Row>
+        </Container>
+    )
 }
