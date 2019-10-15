f05b410cd7f46f1af39a4bf7e34bc6b4463d3f24
scroobius-pip <sim04ful@gmail.com>
2019-10-15 20:00:23 +0100

    added reddit login link

--- components/Modals/LoginModal.tsx
+++ components/Modals/LoginModal.tsx
@@ -1,4 +1,5 @@
 import SocialLoginButton from '@components/SocialLoginButton';
+import Router from 'next/router'
 
 export default ({ errorMessage = '' }: { errorMessage: string }) => {
 
@@ -6,7 +7,7 @@ export default ({ errorMessage = '' }: { errorMessage: string }) => {
         <h2><strong>Login</strong></h2>
         <div style={{ width: '80%', margin: '0 auto', marginTop: 25 }}>
             <span style={{ padding: 10 }}>
-                <SocialLoginButton color='#EA262B' image={require('../../assets/icons/reddit-white.svg')} />
+                <SocialLoginButton color='#EA262B' image={require('../../assets/icons/reddit-white.svg')} onClick={() => window.open("https://swapem-auth.scroobius-pip.now.sh/auth/reddit", "_self")} />
 
             </span>
             <span style={{ padding: 10 }}>
