5274bd235c9081a3d71665ad02223c9597d27010
scroobius-pip <sim04ful@gmail.com>
2020-04-21 00:52:21 +0100

    updated social share

--- components/SocialShare/SocialShare.tsx
+++ components/SocialShare/SocialShare.tsx
@@ -17,10 +17,10 @@ const ButtonProps = {
 
 export default () => {
     return <div style={{ display: 'flex', justifyContent: 'flex-start', marginTop: 10 }}>
-        <FacebookShareButton quote='' hashtag='#radetrade' {...ButtonProps} >
+        <FacebookShareButton quote='https://rade.trade - Create your game wishlist and find gamers that have what you want.'{...ButtonProps} >
             <FacebookIcon {...IconProps} />
         </FacebookShareButton>
-        <TwitterShareButton {...ButtonProps} >
+        <TwitterShareButton hashtags={['gaming']} related={['rade_trade']} title='Rade - Create your game wishlist and find gamers that have what you want.' {...ButtonProps} >
             <TwitterIcon {...IconProps} />
         </TwitterShareButton>
         <RedditShareButton {...ButtonProps}>
